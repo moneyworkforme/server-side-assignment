9ba3a04b35179e0b40b7409c084182babef16bf7
leeteckjunn <leeteckjunn@gmail.com>
2024-03-22 20:37:08 +0800

    added status condition

--- order.php
+++ order.php
@@ -45,6 +45,7 @@ $status = "";
                 <td align="center"><?php echo $row['created_at']; ?></td>
                 <td align="center"><?php echo $currencySymbol . $row['total_price']; ?></td>
                 <td align="center"><?php echo $row['status']; ?></td>
+                <?php if ($row['status'] == "Pending") { ?>
                 <td align="center">
                     <a href="order_view.php?order_ID=<?php echo $row['order_ID']; ?>">View</a>
                     <a href="order_update.php?order_ID=<?php echo $row['order_ID']; ?>">Update</a>
@@ -52,10 +53,14 @@ $status = "";
                         onclick="return confirm('Are you sure you want to delete this order?')">Delete</a>
                 </td>
                 <td align="center">
-                    <a href="order_confirm.php?order_ID=<?php echo $row['order_ID'];?>">Confirm</a>
+                    <a href="order_confirm.php?order_ID=<?php echo $row['order_ID'];?>"
+                        onclick="return confirm('Do you want to confirm the order?')">Confirm</a>
                 </td>
+                <?php } else { ?>
+                    <td></td>
+                    <td></td>
             </tr>
-        <?php } ?>
+        <?php } } ?>
         </tbody>
     </table>
 </body>
