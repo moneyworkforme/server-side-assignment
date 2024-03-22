52c4021a4235ae79641152f94ba691a5a7ea2200
leeteckjunn <leeteckjunn@gmail.com>
2024-03-22 16:42:19 +0800

    new database anme: order_db

--- database.php
+++ database.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("localhost","root","","product_db");
+$con = mysqli_connect("localhost","root","","order_db");
 if (mysqli_connect_error())
 {
     echo "Failed to connect to MySQL: " , mysqli_connect_error();
