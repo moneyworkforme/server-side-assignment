01be01f8f20a1ccfdd0037a3dfecb8c3545aa250
jiasyuen <jiasyuen1426@1utar.my>
2024-03-25 15:28:27 +0800

    main_header done, designing main page

--- product.php
+++ product.php
@@ -8,11 +8,31 @@ require('main_header.php');
     <meta charset="utf-8">
     <title>Product Records</title>
     <link rel="stylesheet" href="css/product.css">
-    
 </head>
 <body>
+    <div class="container">
+        <div>
+            <div class="techkle_big_img">
+                <img src="css/techkle banner.jpeg">
+                <button id="scrollDownBtn">Let's get started!</button>
+            </div>
+            <div id="content">
+                <div class="Charging_Accessories">
+                    <p>Charging Accessories</p>
+                    <div class="line"></div>
+                </div>
+            </div>
+        </div>
+        
 
+    </div>
+    <script>
+        //navigate user with slide down animation to content below
+        document.getElementById("scrollDownBtn").addEventListener("click", function() {
+            document.getElementById("content").scrollIntoView({ behavior: "smooth", block: "start" });
+        });
 
-
+    </script>
 </body>
+
 </html>
